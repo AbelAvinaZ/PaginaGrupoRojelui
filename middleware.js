11fd284c8a08d3361ab267eedb56a2678faa7b5d
AbelAZ <abel.eaz@gmail.com>
2024-05-09 11:37:13 -0600

    actualizacion de pagina

--- middleware.js
+++ middleware.js
@@ -1,3 +1,9 @@
+const { personalSchema, reviewSchema } = require("./schemas");
+const ExpressError = require("./utils/ExpressError");
+const Personal = require("./models/personal");
+const Review = require("./models/review");
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
         req.session.returnTo = req.originalUrl
@@ -12,4 +18,44 @@ module.exports.storeReturnTo = (req, res, next) => {
         res.locals.returnTo = req.session.returnTo;
     }
     next();
+}
+
+module.exports.validatePersonal = (req, res, next) => {
+    const { error } = personalSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(",")
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async(req, res, next) => {
+    const { id } = req.params;
+    const personal = await Personal.findById(id);
+    if (!personal.author.equals(req.user._id)) {
+        req.flash("error", "No tienes permiso para hacer eso!");
+        return res.redirect(`/personal/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash("error", "No tienes permiso para hacer eso!");
+        return res.redirect(`/personal/${id}`);
+    }
+    next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(",")
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
--- routes/personals.js
+++ routes/personals.js
@@ -1,24 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 
 const Personal = require("../models/personal");
 
-const { personalSchema } = require("../schemas");
-const { isLoggedIn } = require("../middleware");
-
-
-const validatePersonal = (req, res, next) => {
-    const { error } = personalSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(",")
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+const { isLoggedIn, isAuthor, validatePersonal } = require("../middleware");
 
 
 router.get("/", catchAsync(async (req, res) => {
@@ -32,37 +19,45 @@ router.get("/new", isLoggedIn, (req, res) => {
 
 router.post("/", isLoggedIn, validatePersonal, catchAsync(async (req, res) => {
     const personal = new Personal(req.body.personal);
+    personal.author = req.user._id;
     await personal.save();
     req.flash("success", "Creaste de forma exitosa a un nuevo empleado!")
     res.redirect(`/personal/${personal._id}`)
 }));
 
 router.get("/:id", catchAsync(async (req, res) => {
-    const personal = await Personal.findById(req.params.id).populate("reviews");
-    if(!personal){
+    const personal = await Personal.findById(req.params.id).populate({
+        path: "reviews",
+        populate: {
+            path: "author"
+        }
+    }).populate("author");
+    console.log(personal);
+    if (!personal) {
         req.flash("error", "No puedo encontrar lo que buscas!");
         return res.redirect("/personal");
     }
     res.render("personal/show", { personal });
 }));
 
-router.get("/:id/edit", isLoggedIn, catchAsync(async (req, res) => {
-    const personal = await Personal.findById(req.params.id)
-    if(!personal){
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const personal = await Personal.findById(id);
+    if (!personal) {
         req.flash("error", "No puedo encontrar lo que buscas!");
         return res.redirect("/personal");
     }
     res.render("personal/edit", { personal });
 }));
 
-router.put("/:id", isLoggedIn, validatePersonal, catchAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, isAuthor, validatePersonal, catchAsync(async (req, res) => {
     const { id } = req.params;
     const personal = await Personal.findByIdAndUpdate(id, { ...req.body.personal });
     req.flash("success", "Modificaste la información del empleado!")
     res.redirect(`/personal/${personal._id}`)
 }));
 
-router.delete("/:id", isLoggedIn, catchAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Personal.findByIdAndDelete(id);
     req.flash("success", "Eliminaste la información del empleado!")
